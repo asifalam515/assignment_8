4a99c7a2a23fee3f2f4d8445e91173c8a9d37421
asifalam515 <asibulalam515@gmail.com>
2024-09-09 22:29:49 +0600

    updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import book from "../../assets/book.jpg";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
     <div className="grid grid-cols-3  m-2 shadow-lg ">
       <div className="banner-text col-span-2 p-20  flex flex-col gap-6 ">
         <h1 className="text-5xl">Books to freshen up your bookshelf</h1>
-        <button className="btn btn-success w-52">View The List</button>
+        <Link to="/listed/" className="btn btn-success w-52">
+          View The List
+        </Link>
       </div>
       <div className="banner-img p-10">
         <img className=" rounded-md" src={book} alt="" />
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,45 +1,21 @@
 import React, { useContext } from "react";
 import { BooksContext } from "../../context/bookContext";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  Cell,
-} from "recharts";
+import { Line, LineChart } from "recharts";
 
 const Chart = () => {
   const { readBooks } = useContext(BooksContext);
 
-  // Create data array for the chart
   const data = readBooks.map((book) => ({
-    name: book.title,
+    name: book.bookName,
     totalPages: book.totalPages,
   }));
-
+  console.log(data.totalPages);
   return (
-    <BarChart width={600} height={300} data={data}>
-      <XAxis dataKey={name} stroke="#8884d8" />
-      <YAxis />
-      <Tooltip wrapperStyle={{ width: 100, backgroundColor: "#ccc" }} />
-      <Legend
-        width={100}
-        wrapperStyle={{
-          top: 40,
-          right: 20,
-          backgroundColor: "#f5f5f5",
-          border: "1px solid #d5d5d5",
-          borderRadius: 3,
-          lineHeight: "40px",
-        }}
-      />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <Bar dataKey="uv" fill="#8884d8" barSize={30} />
-    </BarChart>
+    <div>
+      <LineChart width={500} height={400} data={data.totalPages}>
+        <Line dataKey="book"></Line>
+      </LineChart>
+    </div>
   );
 };
 
